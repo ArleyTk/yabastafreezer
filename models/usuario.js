9d7b03c76b8259ec0927dab809e28ecc4836f46d
ArleyTk <128434157+ArleyTk@users.noreply.github.com>
2023-09-14 15:18:47 -0500

    Add files via upload

---
+++ models/usuario.js
@@ -0,0 +1,32 @@
+const {Schema, model} = require('mongoose')
+
+const PermisoSchema = Schema({
+    idrol: {
+        type: Number,
+        unique: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+
+    nombrerol: {
+        type: String,
+        required: [true, 'el nombre es obligatorio'],
+        minlength: [3, 'Debe tener mínimo 3 caracteres']
+        //maxlength:  [7, 'Debe tener máximo 3 caracteres']
+    },
+
+    descrol: {
+        type: String,
+        required: true,
+    },
+
+    permisosrol: {
+        type: String,
+        required: [true, 'el nombre es obligatorio'],
+        minlength: [3, 'Debe tener mínimo 3 caracteres']
+        //maxlength:  [7, 'Debe tener máximo 3 caracteres']
+    }
+})
+
+//Exportar la función PermisoSchema
+module.exports = model('Permiso',PermisoSchema)
+
